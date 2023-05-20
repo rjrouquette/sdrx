c921113057d97177de339f080e35a2db210bf85f
Robert J. Rouquette <robert.rouquette@focusvq.com>
2023-05-20 15:49:36 -0500

    restructuring code as PTP client

--- tm4c/lib/net.c
+++ tm4c/lib/net.c
@@ -387,7 +387,7 @@ void NET_getTxTime(const uint8_t *txFrame, volatile uint64_t *stamps) {
 
 
 // defined as a weak reference so it may be overriden
-__attribute__((weak))
-void PTP_process(uint8_t *frame, int flen) {
-    __asm volatile("nop");
-}
+//__attribute__((weak))
+//void PTP_process(uint8_t *frame, int flen) {
+//    __asm volatile("nop");
+//}
