e088ef2958de120ebdc5f665b31d1223301b8ab7
Robert J. Rouquette <robert.rouquette@focusvq.com>
2023-05-21 16:26:26 -0500

    restructuring PTP client

--- tm4c/lib/ptp/pll.c
+++ tm4c/lib/ptp/pll.c
@@ -56,11 +56,11 @@ void PLL_updateOffset(const float offset) {
     // large offset delta
     if(fabsf(offset) >= 1e-3f) {
         if(offset < 0) {
-            uint64_t scratch = (uint32_t) -offset;
+            uint64_t scratch = (uint32_t) (-0x1p32f * offset);
             CLK_TAI_adjust((int64_t) -scratch);
             ptpApplyOffset((int64_t) -scratch);
         } else {
-            uint64_t scratch = (uint32_t) offset;
+            uint64_t scratch = (uint32_t) (0x1p32f * offset);
             CLK_TAI_adjust((int64_t) scratch);
             ptpApplyOffset((int64_t) scratch);
         }
