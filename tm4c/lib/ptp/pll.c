3bf7f6d3eedcf4829113c77225f9de06c3ac0a5e
Robert J. Rouquette <robert.rouquette@focusvq.com>
2023-05-23 13:54:57 -0500

    fix flapping on startup

--- tm4c/lib/ptp/pll.c
+++ tm4c/lib/ptp/pll.c
@@ -54,7 +54,7 @@ void PLL_updateOffset(const float offset) {
     }
 
     // large offset delta
-    if(fabsf(offset) >= 1e-3f) {
+    if(fabsf(offset) >= 10e-3f) {
         if(offset < 0) {
             uint64_t scratch = (uint32_t) (-0x1p32f * offset);
             CLK_TAI_adjust((int64_t) -scratch);
